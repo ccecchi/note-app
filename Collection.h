7ff65c64f80f0e02ba8d6045eb0bf21e31113558
carolina <carolinacecchi06@gmail.com>
2025-03-12 17:45:21 +0100

    Getter and Setter for collection

--- Collection.h
+++ Collection.h
@@ -10,6 +10,22 @@ class Collection {
 public:
     explicit Collection(std::string& name): name(name){}
 
+    const std::string &getName() const {
+        return name;
+    }
+
+    void setName(const std::string &name) {
+        Collection::name = name;
+    }
+
+    const std::vector<Note> &getNotes() const {
+        return notes;
+    }
+
+    void setNotes(const std::vector<Note> &notes) {
+        Collection::notes = notes;
+    }
+
 private:
     std::string name;
     std::vector <Note> notes;
